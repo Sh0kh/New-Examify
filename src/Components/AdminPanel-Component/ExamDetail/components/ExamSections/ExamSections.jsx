8f0bf0ad0a9fa5a0eb3bed41cbb51122f5cf8605
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-07-14 18:28:07 +0500

    edit

--- src/Components/AdminPanel-Component/ExamDetail/components/ExamSections/ExamSections.jsx
+++ src/Components/AdminPanel-Component/ExamDetail/components/ExamSections/ExamSections.jsx
@@ -1,6 +1,7 @@
 import { CardBody, Typography, Card, Button } from "@material-tailwind/react";
 import ExamSectionsCreate from "./components/ExamSectionsCreate";
 import { NavLink } from "react-router-dom";
+import ExamSectionEdit from "./components/ExamSectionEdit";
 
 export default function ExamSections({ sectionData, refresh }) {
     return (
@@ -13,18 +14,24 @@ export default function ExamSections({ sectionData, refresh }) {
             </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 !mt-[10px]">
                 {sectionData.map((item, index) => (
-                    <NavLink to={`/o'quv_markaz/imtihon/bolim/${item?.id}`}>
-                        <Card key={index} className="shadow-md border border-gray-300">
-                            <CardBody>
-                                <Typography variant="h5" className="font-semibold mb-2">
-                                    {item?.type}
-                                </Typography>
-                                <Typography className="text-gray-600">{item?.description}</Typography>
-                            </CardBody>
-                        </Card>
-                    </NavLink>
+                    <Card key={index} className="shadow-md border border-gray-300">
+                        <CardBody>
+                            <Typography variant="h5" className="font-semibold mb-2">
+                                {item?.type}
+                            </Typography>
+                            <Typography className="text-gray-600">{item?.description}</Typography>
+                            <div className="flex items-center gap-[10px] mt-[10px]">
+                                <NavLink to={`/o'quv_markaz/imtihon/bolim/${item?.id}`}>
+                                    <Button color="blue">
+                                        Ko'rish
+                                    </Button>
+                                </NavLink>
+                                <ExamSectionEdit section={item} refresh={refresh} />
+                            </div>
+                        </CardBody>
+                    </Card>
                 ))}
-            </div>
+            </div >
         </>
     )
 }
