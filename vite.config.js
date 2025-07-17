1f1581346516c40ac1c2bcff42d489c083f1b621
sh0kh <shoxruuh07tuxtanazarov@gmail.com>
2025-07-17 14:34:48 +0500

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-export default defineConfig({
+const { defineConfig } = require('vite');
+const react = require('@vitejs/plugin-react');
+const path = require('path');
+
+module.exports = defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['some-package'], // замените на имя реальной зависимости
-    },
-  },
-})
+});
